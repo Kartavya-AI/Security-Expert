446cad234e9169a80e1638e0e3d0d1611fef376b
Rajani-07 <drajani3379@gmail.com>
2025-10-20 22:19:57 +0530

    Update crew.py

--- src/security_expert/crew.py
+++ src/security_expert/crew.py
@@ -24,7 +24,7 @@ class SecurityExpertCrew:
             raise ValueError("No Gemini API Key found")
 
         self.llm = ChatGoogleGenerativeAI(
-            model="gemini-2.5-flash",
+            model="google/gemini-2.5-flash",
             verbose=True,
             temperature=0.5,
             google_api_key=os.getenv("GEMINI_API_KEY")
